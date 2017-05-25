f38b3d8d59841c154e6e23c9c5753d556971d92d
ely211 <gonzalesm405@gmail.com>
2017-05-25 17:52:36 -0500

    geolocation zoom 3- laboratoria

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+function initMap(){
+  var laboratoriaLima = {lat: -12.1191427, lng: -77.0349046};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 3,
+    center: laboratoriaLima
+  });
+}
